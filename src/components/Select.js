3659d169d94aecd4c7da95e9e7a8d3cf7c95c8e4
kosar <kosarrajabi@gmail.com>
2022-09-06 03:08:36 -0700

    Changes

---
+++ src/components/Select.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import "../styles/Select.css";
+import axios from "axios";
+
+const Select = ({a}) => {
+  const [select, setSelect] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false"
+      )
+      .then((response) => setSelect(response.data));
+  }, []);
+
+  return (
+    <div className="select-container">
+      <select >
+        {select.map((item) => (
+            
+            <>
+        
+          <option key={item.id}  >{item.name}</option>
+          <option key={item.id} selected={a} >{a}</option>
+          
+          </>
+         
+         
+          
+          
+        ))}
+      </select>
+
+     
+      <i class="fi fi-rs-arrow-right"></i>
+      
+     
+
+      <select>
+        {select.map((item) => (
+          <option key={item.id}>{item.name}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
